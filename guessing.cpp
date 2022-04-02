5d10955d92cf126a2835db273824266113a090dd
Ubuntu <ubuntu@ip-172-31-21-22.ec2.internal>
2022-04-02 15:36:47 +0000

    upgraded my code :)

--- guessing.cpp
+++ guessing.cpp
@@ -9,28 +9,36 @@
 #include <random>
 
 int main() {
-    // This function takes a random integer between 0-9
-    // and tells the user if they guessed corretly
-    int number;
-    int user_guess;
+    // this function checks for the random number
+    int guessed_as_number;
+    std::string guessed_input;
+    int random_guess;
 
     std::random_device rseed;
-    std::mt19937 rgen(rseed());  // mersenne_twister
-    std::uniform_int_distribution<int> idist(0, 9);  // [0,9]
-    number = idist(rgen);
+    std::mt19937 rgen(rseed());
+    // mersence_twister
+    std::uniform_int_distribution<int> idist(0, 9);
+    // [0, 9]
+    random_guess = idist(rgen);
 
     // input
-    std::cout << "Enter a random number between 0-9 (integer): ";
-    std::cin >> user_guess;
+    std::cout << "Enter a random number between 0 and 9 (integer): ";
+    std::cin >> guessed_input;
     std::cout << "" << std::endl;
 
     // process and output
-    if (user_guess == number) {
-        std::cout << "Hooray you guessed correctly !! :)" << std::endl;
-    } else {
-    std::cout << "Oh No!!! guessed incorrectly :("
-        << std::endl;
+    try {
+        guessed_as_number = std::stoi(guessed_input);
+        if (guessed_as_number == random_guess) {
+            std::cout << "Correct" << std::endl;
+        } else {
+            std::cout << "Wrong the number is "
+                     << random_guess << std::endl;
+        }
     }
-    std::cout << "" << std::endl;
-    std::cout << "\nDone.";
+    catch (std::invalid_argument) {
+        std::cout << "Really? (-_-)ゞ pick a Integer pls." << std::endl;
+    }
+
+    std::cout << "\nDone." << std::endl;
 }
